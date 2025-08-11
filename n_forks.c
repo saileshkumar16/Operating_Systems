4386e4d349263bf5dab3d9a9e6af600813f9e66a
Sailesh Kumar Pasam <144588637+saileshkumar16@users.noreply.github.com>
2025-08-12 00:02:49 +0530

    Add files via upload

---
+++ n_forks.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <unistd.h>
+
+int main() 
+{
+    
+    
+    fork(); // will create exact duplicate and point to next line, 
+    fork(); // will create exact duplicate and iteratte it back, total if n forks 2 power n processes will be created
+   
+    printf("Hi");
+
+    return 0;
+}
