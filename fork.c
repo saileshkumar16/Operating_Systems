48b5019dba88cf5b9629022cc5a6bfe4701c06d9
Sailesh Kumar Pasam <144588637+saileshkumar16@users.noreply.github.com>
2025-08-11 23:50:29 +0530

    Add files via upload

---
+++ fork.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <unistd.h>
+
+int main() 
+{
+    int a,b,c;
+    c = a+b;
+    
+    fork();
+    printf("Hi");
+
+    return 0;
+}
